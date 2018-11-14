9b653f7ebe0b3ff54d9fcc013db264b5b89bbcbf
Malcolm Lambrecht <jml1769@rit.edu>
2018-11-14 13:13:38 -0500

    created basic textures, created template for new tiles, super basic generation

---
+++ Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour {
+
+    public List<GameObject> TileTypes;
+    public List<Sprite> TileSprites;
+    public int xSize, ySize;
+    public int tileSize = 64;
+    private GameObject[,] tilemap;
+
+	// Use this for initialization
+	void Start () {
+        GenerateTilemap(5,5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GenerateTilemap(int x, int y)
+    {
+        tilemap = new GameObject[x, y];
+        xSize = x;
+        ySize = y;
+        PlaceWalls();
+    }
+
+    void PlaceWalls()
+    {
+        for(int x= 0; x < xSize; x++)
+        {
+            for(int y = 0; y < ySize; y++)
+            {
+               
+                tilemap[x, y] = Instantiate(TileTypes[0], gameObject.transform);
+                Tile_Wall t = tilemap[x, y].GetComponent<Tile_Wall>();
+                t.myTexture = TileSprites[0];
+                t.xIndex = x;
+                t.yIndex = y;
+                tilemap[x, y].transform.position = new Vector3(x, y, 0.0f);
+                t.RunInit();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tile Types/Tile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * ALL TILES MUST HAVE SPRITE AND ROTATION SET BY THE LEVEL GENERATOR
+ * ANY TILES THAT NEED EXTRA FUNCTIONALITY SHOULD EXTEND THIS SCRIPT
+ * ANY TILES THE LEVEL GENERATOR NEEDS TO KNOW ABOUT SHOULD BE ADDED TO ITS TILE TYPES PROPERTY IN THE INSPECTOR
+ * LOOK AT Tile_Wall for how to do this
+ */
+public class Tile : MonoBehaviour {
+
+    public Sprite myTexture;
+    public float myRotation = 0.0f;
+    public string layerName;
+    public int xIndex;
+    public int yIndex;
+	// Use this for initialization
+	protected virtual void Start () {
+
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		// this probably shouldnt have anything in it bc it will affect every tile
+	}
+
+    protected virtual void Init()
+    {
+        SpriteSetup();
+        SetLayer(layerName);
+    }
+
+    public void SpriteSetup()
+    {
+        SpriteRenderer spriteRender = gameObject.GetComponent<SpriteRenderer>();
+        spriteRender.sprite = myTexture;
+        gameObject.transform.Rotate(new Vector3(0.0f,0.0f,myRotation));//only rotate on one axis bc 2d
+    }
+
+    public void SetLayer(string newLayer)
+    {
+        int l = LayerMask.NameToLayer(newLayer);
+        if(l == -1) //should check for an error, this is to keep away making dumb typos lol
+        {
+            Debug.LogError(gameObject + " layer not found: " + newLayer);
+        }
+        gameObject.layer = l;
+    }
+
+}
