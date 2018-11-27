36f511d039ab5bdaef5505b344760b59cb02cd92
David Knolls <Therobodavo@gmail.com>
2018-11-27 15:17:20 -0500

    spike trap

---
+++ Assets/Scripts/Tile Types/Tile_Spikes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile_Spikes : Tile {
+
+	private GameObject player;
+
+	// Use this for initialization
+	protected override void Start()
+	{
+		base.Start();
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	protected override void Update()
+	{
+		base.Update();
+	}
+
+	protected override void Init()
+	{
+		base.Init();
+	}
+
+	public void RunInit() //this is dumb and only so init can be public too
+	{
+		Init();
+	}
+	public void OnCollisionEnter(Collision collision)
+	{
+		//trigger trap
+		player.transform.position = new Vector3(0,0,0);
+	}
+
+}
