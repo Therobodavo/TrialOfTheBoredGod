19d4c972e6b59aed9b99008c281517fe13930fd1
mro5772 <mro5772@IGM2025-13>
2018-11-26 20:17:24 -0500

    making menu snazzy

---
+++ Assets/Menu/UiScripts/ButtonControls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonControls : MonoBehaviour {
+
+  public  Animator animator1;
+    public Animator animator2;
+    public Animator animator3;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void hideButtons()
+    {
+        animator1.SetBool("isHide", true);
+        animator2.SetBool("isHide", true);
+        animator3.SetBool("isHide", true);
+    }
+    public void showButtons()
+    {
+        animator1.SetBool("isHide", false);
+        animator2.SetBool("isHide", false);
+        animator3.SetBool("isHide", false);
+    }
+}
