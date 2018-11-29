d7d98a76cd087abf127fcb19b3592e457c497a23
Malcolm Lambrecht <jml1769@rit.edu>
2018-11-29 13:28:33 -0500

    Created skeleton classes for the trap types

---
+++ Assets/Scripts/Tile Types/Tile_SpearTrap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile_SpearTrap : Tile
+{
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void Init()
+    {
+        base.Init();
+    }
+
+    public void RunInit() //this is dumb and only so init can be public too
+    {
+        Init();
+    }
+}
