1f9b059741e096803a2a942e6aee66805c78e239
Tristan Marshall <tam8217@g.rit.edu>
2018-12-09 15:05:36 -0500

    Basic spear trap algorith

--- Assets/Scripts/Tile Types/Tile_SpearTrap.cs
+++ Assets/Scripts/Tile Types/Tile_SpearTrap.cs
@@ -4,17 +4,51 @@ using UnityEngine;
 
 public class Tile_SpearTrap : Tile
 {
+    CircleCollider2D outerCirc;
 
+    float startTime = 0.0f;
+    public float attackDelay = 2.0f;
+    bool hasStarted = false;
+    bool attack = false;
+
+    float attackLinger = 1.0f;
+    bool attackLingerBool = false;
+
+    public GameObject player;
     // Use this for initialization
     protected override void Start()
     {
         base.Start();
+
+        outerCirc = GetComponent<CircleCollider2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     protected override void Update()
     {
         base.Update();
+
+        if(hasStarted || attackLingerBool)
+        {
+            startTime += Time.deltaTime;
+        }
+
+        if( startTime >= attackDelay && hasStarted)
+        {
+            hasStarted = false;
+            attack = true;
+            startTime = 0.0f;
+            Debug.Log("Starting attack");
+            attackLingerBool = true;
+        }
+        if(startTime >= attackLinger && attackLingerBool)
+        {
+            attackLingerBool = false;
+            attack = false;
+            startTime = 0.0f;
+            Debug.Log("Ending attack");
+        }
     }
 
     protected override void Init()
@@ -26,4 +60,36 @@ public class Tile_SpearTrap : Tile
     {
         Init();
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (hasStarted == false)
+        {
+            hasStarted = true;
+            Debug.Log("Initial contact");
+        }
+    }
+    private void OnTriggerStay2D(Collider collision)
+    {
+        if (attack)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                collision.gameObject.transform.position = new Vector3(0, 0, 0);
+                player.transform.position = new Vector3(0, 0, 0);
+
+            }
+
+        }
+    }
+    /* private void OnTriggerEnter2D(Collider other)
+     {
+
+     }
+
+     private void OnTriggerStay2D(Collider other)
+     {
+         
+     }
+     */
 }
