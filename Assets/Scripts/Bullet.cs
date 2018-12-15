1f4c4e806ee6b8e3b0857c471955d6f4e81cff70
David Knolls <Therobodavo@gmail.com>
2018-12-15 16:34:07 -0500

    set up turret

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public GameObject shooter;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+    player = GameObject.FindGameObjectWithTag ("Player");
+        if(shooter != null)
+		    this.transform.parent.rotation = shooter.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	      this.transform.parent.position += new Vector3(this.transform.parent.up.x, this.transform.parent.up.y,0) * 3 * Time.deltaTime;	
+	}
+    void OnTriggerEnter2D(Collider2D col)
+	{
+        if (col.gameObject == player)
+        {
+            player.GetComponent<Move>().isDead = true;
+            player.GetComponent<Move>().killed = "Turret";
+        }
+        else if(col.gameObject.tag == "Wall")
+        {
+            Destroy(this);
+        }
+    }
+}
--- Assets/Scripts/Tile Types/Tile_Turret.cs
+++ Assets/Scripts/Tile Types/Tile_Turret.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class Tile_Turret : Tile
 {
+    private GameObject player;
+    public GameObject bullet;
+    float lastShot = 0;
+    float delay = 0.8f;
 
     // Use this for initialization
     protected override void Start()
     {
         base.Start();
+        player = GameObject.FindGameObjectWithTag ("Player");
     }
 
     // Update is called once per frame
     protected override void Update()
     {
         base.Update();
+
+        //Get Vector between turret and player
+        Vector3 dir = player.transform.position - this.transform.position;
+
+        //if distance between turret and player is greater than 8 units
+        if(dir.magnitude < 8f)
+        {
+            //Get the angle from the turret to the player
+            float dirAngle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, dirAngle - 90);
+
+            //Shot delay
+            if(Time.timeSinceLevelLoad - lastShot >= delay)
+            {
+                lastShot = Time.timeSinceLevelLoad;
+
+                //Creates bullet
+                bullet.transform.position = new Vector3(this.transform.position.x + this.transform.up.x, this.transform.position.y + this.transform.up.y, 0);
+                bullet.transform.GetChild(0).GetComponent<Bullet>().shooter = this.gameObject;
+                GameObject bul = Instantiate(bullet);
+            }
+        }
     }
 
     protected override void Init()
